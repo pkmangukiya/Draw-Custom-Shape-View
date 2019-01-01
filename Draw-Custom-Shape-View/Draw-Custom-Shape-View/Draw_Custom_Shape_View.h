fab3cb739732e295c3be9159785f1fdc4b86f192
Hiten iMac <bbt.pareshk@gmail.com>
2019-01-01 16:19:28 +0530

    Finished our Draw-Custom-Shape-View

---
+++ Draw-Custom-Shape-View/Draw-Custom-Shape-View/Draw_Custom_Shape_View.h
@@ -0,0 +1,19 @@
+//
+//  Draw_Custom_Shape_View.h
+//  Draw-Custom-Shape-View
+//
+//  Created by Hiten iMac on 01/01/19.
+//  Copyright © 2019 Hiten iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Draw_Custom_Shape_View.
+FOUNDATION_EXPORT double Draw_Custom_Shape_ViewVersionNumber;
+
+//! Project version string for Draw_Custom_Shape_View.
+FOUNDATION_EXPORT const unsigned char Draw_Custom_Shape_ViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Draw_Custom_Shape_View/PublicHeader.h>
+
+
---
+++ Draw-Custom-Shape-View/Draw-Custom-Shape-View/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  Draw-Custom-Shape-View
+//
+//  Created by Hiten iMac on 01/01/19.
+//  Copyright © 2019 Hiten iMac. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did Some Stuff"
+    }
+}
